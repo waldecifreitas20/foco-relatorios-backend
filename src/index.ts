30436298185b7c0b5630e9f42f59f3b8d495fed0
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-14 10:57:04 -0300

    fix: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 import { appRouter } from "./core/routes/routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: "*",
+}))
 
 app.get("/", (req, res) => {
   return res.status(200).json({ msg: 'ok' });
