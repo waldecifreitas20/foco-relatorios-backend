a5eef61b341c8bfc2aaecb7089536d8c20aea00f
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-17 18:26:05 -0300

    fix: index.js imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { appRouter } from "./core/routes/routes";
+import { appRouter } from "./core/routes/routes.js";
 
 const app = express();
 
