ca3cf1cd49d8d37843d0e5ea4c5f74375f6748b9
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-01-23 17:23:44 -0300

    refactor: delete minimal  special budget creation 2

--- src/dto/specialbudget.dto.ts
+++ src/dto/specialbudget.dto.ts
@@ -3,37 +3,19 @@ import { Service } from "../types/Service";
 import { SpecialBudgetReason, SpecialBudgetStatus } from "../types/SpecialBudget";
 
 export interface CreateSpecialBudgetDto {
-  protocol: string;
-  cost: number;
-  status: SpecialBudgetStatus;
-  reason: SpecialBudgetReason;
-
-  wheelDollies?: number;
-  additionalWheels?: number;
-  daysParked?: number;
-  isUprighted?: boolean;
-  isOffRoad?: boolean;
-  needGroundWithdraw?: boolean;
-  origin?: string;
-  destiny?: string;
-  workerBase?: string;
-  explanation?: string;
-}
-
-
-export interface CreateMinimalSpecialBudgetDto {
   cost: number;
   status: SpecialBudgetStatus;
   protocol: string;
+  reason?: string;
 }
 
 
 export interface UpdateSpecialBudgetDto {
   id: number;
-  cost: number;
-  status: SpecialBudgetStatus;
+  cost?: number;
   protocol: string;
-  reason: string;
+  status?: SpecialBudgetStatus;
+  reason?: string;
 }
 
 
